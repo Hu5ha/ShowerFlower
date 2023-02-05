8be073d04c46763d44e20de7585551e9a9e6e941
LAPTOP-TFQVDDGS\inuch <nathalie.senechal@mailbox.tu-dresden.de>
2023-02-05 14:27:36 +0100

    carpet tree

--- Assets/Scripts/DecorationController.cs
+++ Assets/Scripts/DecorationController.cs
@@ -8,6 +8,7 @@ public class DecorationController : MonoBehaviour
     [SerializeField] private GameObject flower;
     [SerializeField] private Vector3 position_origin;
     private InputEvent inputEvent;
+    private float y_value = 1 ;
 
     // Start is called before the first frame update
     void Start()
@@ -18,15 +19,15 @@ public class DecorationController : MonoBehaviour
 
     private void Decorate(float position)
     {
-        
-       Instantiate(flower, new Vector3(0,inputEvent.GetBranchEndPosition(),-2), Quaternion.Euler(-90, 0, 0));
+
+        Instantiate(flower, new Vector3(0, inputEvent.GetBranchEndPosition(), -2), Quaternion.Euler(-90, 0, 0));
         Instantiate(flower, GenerateRandomVector(), Quaternion.Euler(-90, 0, 0));
 
     }
 
     private Vector3 GenerateRandomVector()
     {
-        float y_value = inputEvent.time_position.GetCurrentTimerValue();
+        y_value++;
         float x_value = Random.Range(-10, 10);
         return new Vector3(x_value, y_value, -3);
     }
